1b6f40ec0d9e2d979223cebc4953ec7bc44038fd
TracingInsights-Archive <153351807+TracingInsights-Archive@users.noreply.github.com>
2023-12-25 09:29:07 +0530

    Update tel.py

--- tel.py
+++ tel.py
@@ -23,8 +23,8 @@ events = [#'Australian Grand Prix',
     # 'Canadian Grand Prix', 'French Grand Prix', 'Austrian Grand Prix', 
     # 'British Grand Prix', 'German Grand Prix', 'Hungarian Grand Prix', 
     # 'Belgian Grand Prix', 'Italian Grand Prix', 'Singapore Grand Prix', 
-           'Russian Grand Prix', 'Japanese Grand Prix', 'Mexican Grand Prix', 
-    # 'United States Grand Prix', 'Brazilian Grand Prix', 'Abu Dhabi Grand Prix'
+           # 'Russian Grand Prix', 'Japanese Grand Prix', 'Mexican Grand Prix', 
+    'United States Grand Prix', 'Brazilian Grand Prix', 'Abu Dhabi Grand Prix'
           ]
          
 
